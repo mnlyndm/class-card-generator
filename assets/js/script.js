5acfe0e8e566128c6e2abe8bda622cc8cc1cc6c1
mnlyndm <millaminaminalyn@gmail.com>
2023-11-16 01:02:52 +0800

    Generated calss card design added

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,28 +15,119 @@ document.getElementById('generate-button').addEventListener('click', function ()
       classCards.innerHTML = '';
 
       studentData.forEach(student => {
-        const lastName = student['Last Name'] || '';
-        const firstName = student['First Name'] || '';
-        const middleName = student['Middle Name'] || '';
-        const level = student.Level || '';
-        const course = student.Course || '';
-        const subject = student.Subject || '';
-        const professor = student.Professor || '';
-        const day = student.Day || '';
-        const time = student.Time || '';
-        const rating = student['Final Rating'] || '';
+        const lastName = student['Last Name'] || '&nbsp;';
+        const firstName = student['First Name'] || '&nbsp;';
+        const middleName = student['Middle Name'] || '&nbsp;';
+        const level = student.Level || '&nbsp;';
+        const course = student.Course || '&nbsp;';
+        const subject = student.Subject || '&nbsp;';
+        const professor = student.Professor || '&nbsp;';
+        const day = student.Day || '&nbsp;';
+        const time = student.Time || '&nbsp;';
+        const rating = student['Final Rating'] || '&nbsp;';
 
         const card = `
-          <div class="bg-white p-4 rounded shadow-lg mb-4">
-            <h2 class="text-xl font-semibold">${lastName}, ${firstName} ${middleName}</h2>
-            <p>Level: ${level}</p>
-            <p>Course: ${course}</p>
-            <p>Subject: ${subject}</p>
-            <p>Professor: ${professor}</p>
-            <p>Day: ${day}</p>
-            <p>Time: ${time}</p>
-            <p>Final Rating: ${rating}</p>
+        <div class="flex flex-col justify-start w-full items-center bg-white border border-gray-400 p-1 mt-2 rounded-sm mb-4" style="width: 336px; height: 192px; ">
+        <div>
+          <img src="./assets/svg/ollclogo.svg" class="w-40" alt="">
+        </div>
+        <div style="color: #0D1158; font-size: 8px;">
+          __ Semester, 20__-20__
+        </div>
+        <div class=" font-bold" style="color: #0D1158; font-size: 12px;">
+          CLASS CARD
+        </div>
+        <div class=" w-full px-2 text-xs mt-1 inline-flex justify-start items-center gap-4" style="color: #0D1158; font-size: 8px;">
+          <div class="text-center w-full">
+            <div class="border border-solid text-black" style="border-color: #0D1158; padding-left: 4px; padding-right: 4px;">
+              ${lastName} 
+            </div>
+            <div>
+              Surname
+            </div>
           </div>
+          <div class="text-center w-full">
+            <div class="border border-solid text-black" style="border-color: #0D1158; padding-left: 4px; padding-right: 4px;">
+              ${firstName} 
+            </div>
+            <div>
+              First Name
+            </div>
+          </div>
+          <div class="text-center w-full">
+            <div class="border border-solid text-black" style="border-color: #0D1158; padding-left: 4px; padding-right: 4px;">
+              ${middleName} 
+            </div>
+            <div>
+              Middle Name
+            </div>
+          </div>
+        </div>
+        <div class=" w-full px-2 text-xs mt-1 inline-flex justify-start items-center gap-4" style="color: #0D1158; font-size: 8px;">
+          <div class="text-center w-full">
+            <div class="border border-solid text-black" style="border-color: #0D1158; padding-left: 4px; padding-right: 4px;">
+              ${level} 
+            </div>
+            <div>
+              Level
+            </div>
+          </div>
+          <div class="text-center w-full">
+            <div class="border border-solid text-black" style="border-color: #0D1158; padding-left: 4px; padding-right: 4px;">
+              ${course} 
+            </div>
+            <div>
+              Course
+            </div>
+          </div>
+          <div class="text-center w-full">
+            <div class="border border-solid text-black" style="border-color: #0D1158; padding-left: 4px; padding-right: 4px;">
+              ${subject} 
+            </div>
+            <div>
+              Subject
+            </div>
+          </div>
+        </div>
+        <div class=" w-full px-2 text-xs mt-1 inline-flex justify-start items-center gap-4" style="color: #0D1158; font-size: 8px;">
+          <div class="text-center w-full">
+            <div class="border border-solid text-black" style="border-color: #0D1158; padding-left: 4px; padding-right: 4px;">
+              ${professor} 
+            </div>
+            <div>
+              Professor
+            </div>
+          </div>
+          <div class="text-center w-full">
+            <div class="flex justify-center items-center gap-1">
+              <div>
+                <div class="border border-solid text-black" style="border-color: #0D1158; padding-left: 4px; padding-right: 4px;">
+                  ${day}
+                </div>
+                <div>
+                  Day
+                </div>
+              </div>
+              <div>
+                <div class="border border-solid text-black" style="border-color: #0D1158; padding-left: 4px; padding-right: 4px;">
+                  ${time} 
+                </div>
+                <div>
+                  Time
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="text-center w-full">
+            <div class="border border-solid text-black" style="border-color: #0D1158; padding-left: 4px; padding-right: 4px;">
+              ${rating} 
+            </div>
+            <div>
+              Final Rating
+            </div>
+          </div>
+        </div>
+      <div>
         `;
 
         const cardContainer = document.createElement('div');
@@ -45,7 +136,7 @@ document.getElementById('generate-button').addEventListener('click', function ()
         cardContainer.innerHTML = card;
 
         const downloadButton = document.createElement('button');
-        downloadButton.innerHTML = '<i class="fa fa-download"></i> Download Card';
+        downloadButton.innerHTML = 'Download Card';
         downloadButton.addEventListener('click', function () {
           downloadCardAsImage(cardContainer);
         });
